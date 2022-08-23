49d46db0edb9ad61f35ded2f614b8017da7ce084
Sima2204 <vlad.simuta@gmail.com>
2022-08-23 22:08:06 +0300

    fix: import done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,1 +1,8 @@
-console.log('easy');
+import ancientsData from './data/ancients.js';
+import difficulties from './data/difficulties.js';
+import mythicCards from './data/mythicCards/index.js';
+import blueMythicCards from './data/mythicCards/blue/index.js';
+import brownMythicCards from './data/mythicCards/brown/index.js';
+import greenMythicCards from './data/mythicCards/green/index.js';
+
+console.log(ancientsData);

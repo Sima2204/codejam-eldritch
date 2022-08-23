139886e6ad389732cefdae2f8764995c757e41e1
Sima2204 <vlad.simuta@gmail.com>
2022-08-23 23:19:06 +0300

    init: index.js started, added total cards counter

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,4 +5,17 @@ import {
   blueCards,
   greenCards
 } from './data/mythicCards/index.js';
+import cardsDataBrown from './data/mythicCards/brown/index.js'
+import cardsDataGreen from './data/mythicCards/green/index.js'
+import cardsDataBlue from './data/mythicCards/blue/index.js'
+
+
+function totalCards() {
+  let green = ancientsData[0].firstStage.greenCards + ancientsData[0].secondStage.greenCards + ancientsData[0].thirdStage.greenCards;
+  let brown = ancientsData[0].firstStage.brownCards + ancientsData[0].secondStage.brownCards + ancientsData[0].thirdStage.brownCards;
+  let blue = ancientsData[0].firstStage.blueCards + ancientsData[0].secondStage.blueCards + ancientsData[0].thirdStage.blueCards;
+}
+totalCards();
+
+console.log(cardsDataGreen);
 

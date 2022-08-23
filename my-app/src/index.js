1f5f3e3f905a48e1ba656c6f4d85d2b36bf94ffc
Sima2204 <vlad.simuta@gmail.com>
2022-08-23 22:40:31 +0300

    fix: html+css

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
 import ancientsData from './data/ancients.js';
 import difficulties from './data/difficulties.js';
-import mythicCards from './data/mythicCards/index.js';
-import blueMythicCards from './data/mythicCards/blue/index.js';
-import brownMythicCards from './data/mythicCards/brown/index.js';
-import greenMythicCards from './data/mythicCards/green/index.js';
+import {
+  brownCards,
+  blueCards,
+  greenCards
+} from './data/mythicCards/index.js';
 
-console.log(ancientsData);

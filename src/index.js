4590b65a4662c72ecfc688da71089ebbb1e8985e
Sima2204 <vlad.simuta@gmail.com>
2022-08-25 23:17:39 +0300

    fix: all done at 25.08.2022

--- src/index.js
+++ src/index.js
@@ -1,121 +1,116 @@
 import ancientsData from "./data/ancients.js";
 import difficulties from "./data/difficulties.js";
 import { brownCards, blueCards, greenCards } from "./data/mythicCards/index.js";
-import cardsDataBrown from "./data/mythicCards/brown/index.js";
-import cardsDataGreen from "./data/mythicCards/green/index.js";
-import cardsDataBlue from "./data/mythicCards/blue/index.js";
+import { shuffle } from "./helper.js";
 
-// количество всех карт всех уровней по цветам
-let green =
-  ancientsData[0].firstStage.greenCards +
-  ancientsData[0].secondStage.greenCards +
-  ancientsData[0].thirdStage.greenCards;
-let brown =
-  ancientsData[0].firstStage.brownCards +
-  ancientsData[0].secondStage.brownCards +
-  ancientsData[0].thirdStage.brownCards;
-let blue =
-  ancientsData[0].firstStage.blueCards +
-  ancientsData[0].secondStage.blueCards +
-  ancientsData[0].thirdStage.blueCards;
+const DIFFICULTY_LEVEL_NORMAL = 'normal';
+const AZATHOTH_INDEX = 0;
 
-// собираем все зеленые карты normal
-let greenCardsNormalLevel = [];
-function collectGreenCards() {
-  for (let i = 0; i < cardsDataGreen.length; i++) {
-    if (greenCards[i].difficulty === "normal") {
-      greenCardsNormalLevel.push(greenCards[i]);
-    }
-  }
+// добавляем цифры в счетчике
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+
+setValues(ancientsData, AZATHOTH_INDEX);
+
+const cardsQuantity = getCardsQuantity(ancientsData, AZATHOTH_INDEX);
+
+const allCards = getAllCards(greenCards, blueCards, brownCards, DIFFICULTY_LEVEL_NORMAL, cardsQuantity);
+
+const finalCards = getFinalCards(allCards, ancientsData, AZATHOTH_INDEX);
+
+function getCardsQuantity(ancientsData, ancientIndex) {
+  const cardsQuantity = [0, 0, 0];
+
+  const firstStageValues = Object.values(ancientsData[ancientIndex].firstStage);
+  const secondStageValues = Object.values(ancientsData[ancientIndex].secondStage);
+  const thirdStageValues = Object.values(ancientsData[ancientIndex].thirdStage);
+
+  return cardsQuantity.map((value, index) => value + firstStageValues[index] + secondStageValues[index] + thirdStageValues[index]);
 }
-collectGreenCards();
-// console.log(greenCardsNormalLevel);
 
-// собираем все коричневые карты normal
-let brownCardsNormalLevel = [];
-function collectBrownCards() {
-  for (let i = 0; i < cardsDataBrown.length; i++) {
-    if (brownCards[i].difficulty === "normal") {
-      brownCardsNormalLevel.push(brownCards[i]);
-    }
-  }
-}
-collectBrownCards();
-// console.log(brownCardsNormalLevel);
+function getAllCards(greenCards, blueCards, brownCards, difficulty, cardsQuantity) {
+  return {
+    greenCards: getCardsSetByColorAndDifficulty(greenCards, difficulty, cardsQuantity[0]),
+    blueCards: getCardsSetByColorAndDifficulty(blueCards, difficulty, cardsQuantity[1]),
+    brownCards: getCardsSetByColorAndDifficulty(brownCards, difficulty, cardsQuantity[2])
+  };
+};
 
-// собираем все синие карты normal
-let blueCardsNormalLevel = [];
-function collectBlueCards() {
-  for (let i = 0; i < cardsDataBlue.length; i++) {
-    if (blueCards[i].difficulty === "normal") {
-      blueCardsNormalLevel.push(blueCards[i]);
-    }
-  }
-}
-collectBlueCards();
-// console.log(blueCardsNormalLevel);
-
-// собираем карты на все три стейджа normal
-const shuffledGreen = greenCardsNormalLevel.sort(() => 0.5 - Math.random());
-let selectedGreen = shuffledGreen.slice(0, 5);
-// console.log(selectedGreen);
-
-const shuffledBlue = blueCardsNormalLevel.sort(() => 0.5 - Math.random());
-let selectedBlue = shuffledBlue.slice(0, 2);
-// console.log(selectedBlue);
-
-const shuffledBrown = brownCardsNormalLevel.sort(() => 0.5 - Math.random());
-let selectedBrown = shuffledBrown.slice(0, 9);
-// console.log(selectedBrown);
+// функция, которая принимает массив по цвету, фильтрует его по сложности, и выводит перемешанный массив нужного количества
+function getCardsSetByColorAndDifficulty(color, difficulty, quantity) {
+  return shuffle(color.filter(card => card.difficulty === difficulty)).slice(0, quantity);
+};
 
 // распределение по стейджам
-let firstStageCards = [];
-firstStageCards.push(selectedGreen[0]);
-firstStageCards.push(selectedBlue[0]);
-firstStageCards.push(selectedBrown[0]);
-firstStageCards.push(selectedBrown[1]);
+function getFinalCards(cards, ancientsData, ancientIndex) {
+  const firstStageCards = getCardsByStage(cards, ancientsData[ancientIndex].firstStage);
+  const secondStageCards = getCardsByStage(cards, ancientsData[ancientIndex].secondStage);
+  const thirdStageCards = getCardsByStage(cards, ancientsData[ancientIndex].thirdStage);
 
-let secondStageCards = [];
-secondStageCards.push(selectedGreen[1]);
-secondStageCards.push(selectedGreen[2]);
-secondStageCards.push(selectedBlue[1]);
-secondStageCards.push(selectedBrown[2]);
-secondStageCards.push(selectedBrown[3]);
-secondStageCards.push(selectedBrown[4]);
+  return thirdStageCards.concat(secondStageCards, firstStageCards);
+};
 
-let thirdStageCards = [];
-thirdStageCards.push(selectedGreen[3]);
-thirdStageCards.push(selectedGreen[4]);
-thirdStageCards.push(selectedBrown[5]);
-thirdStageCards.push(selectedBrown[6]);
-thirdStageCards.push(selectedBrown[7]);
-thirdStageCards.push(selectedBrown[8]);
+function getCardsByStage(cards, stage) {
+  return shuffle(cards.greenCards.splice(0, stage.greenCards).concat(cards.blueCards.splice(0, stage.blueCards), cards.brownCards.splice(0, stage.brownCards)));
+};
 
-// перемешиваем три колоды
-function shuffle(arr) {
-  arr.sort(() => Math.random() - 0.5);
-}
+// устанавливаем значения в стейдж
 
-shuffle(firstStageCards);
-shuffle(secondStageCards);
-shuffle(thirdStageCards);
-// console.log(firstStageCards);
-// console.log(secondStageCards);
-// console.log(thirdStageCards);
+function setValues(ancientsData, ancientIndex) {
+  one.innerHTML = ancientsData[ancientIndex].firstStage.greenCards;
+  two.innerHTML = ancientsData[ancientIndex].firstStage.blueCards;
+  three.innerHTML = ancientsData[ancientIndex].firstStage.brownCards;
+  four.innerHTML = ancientsData[ancientIndex].secondStage.greenCards;
+  five.innerHTML = ancientsData[ancientIndex].secondStage.blueCards;
+  six.innerHTML = ancientsData[ancientIndex].secondStage.brownCards;
+  seven.innerHTML = ancientsData[ancientIndex].thirdStage.greenCards;
+  eight.innerHTML = ancientsData[ancientIndex].thirdStage.blueCards;
+  nine.innerHTML = ancientsData[ancientIndex].thirdStage.brownCards;
+};
 
-// собираем одну колоду
-let finalCards = thirdStageCards.concat(secondStageCards, firstStageCards);
-console.log(finalCards);
+// достаем карты по очереди по одной, убираем из колоды, отнимаем число
 
-// достаем карты по очереди по одной и убираем из колоды
 function showCard() {
-  if (finalCards.length != 0) {
-    let element = finalCards.pop();
+  const frontImage = document.querySelector('.card_opened_now');
+  const backImage = document.querySelector('.card_back_side');
+
+  if (finalCards.length) {
+    const element = finalCards.pop();
     console.log(element);
+
+    frontImage.src = element.cardFace;
+
+    if (element.id.includes('green')) {
+      delValues(one, four, seven);
+    } else if (element.id.includes('blue')) {
+      delValues(two, five, eight);
+    } else if (element.id.includes('brown')) {
+      delValues(three, six, nine);
+    }
+
   } else {
+    backImage.style.display = 'none';
     console.log('here are no more cards left')
   }
-}
+};
 
 const cardBackSide = document.querySelector(".card_back_side");
 cardBackSide.addEventListener("click", showCard);
+
+// удаляем цифры в счетчике
+function delValues(green, blue, brown) {
+  if (+green.innerHTML) {
+    green.innerHTML -= 1;
+  } else if (+blue.innerHTML) {
+    blue.innerHTML -= 1;
+  } else {
+    brown.innerHTML -= 1;
+  }
+};

47cd23905fede10e1860ebd57c182fd3c9bded32
Sima2204 <vlad.simuta@gmail.com>
2022-08-26 13:32:48 +0300

    init: working with clicks

--- src/index.js
+++ src/index.js
@@ -1,121 +1,112 @@
-import ancientsData from "./data/ancients.js";
-import difficulties from "./data/difficulties.js";
-import { brownCards, blueCards, greenCards } from "./data/mythicCards/index.js";
-import { shuffle } from "./helper.js";
+import ancientsData from './data/ancients.js';
+import difficulties from './data/difficulties.js';
+import { brownCards, blueCards, greenCards } from './data/mythicCards/index.js';
+import { getCardsQuantity, getAllCards, getFinalCards } from './helper.js';
 
 const DIFFICULTY_LEVEL_NORMAL = 'normal';
+const AZATHOTH = document.getElementById('first-monster');
+const CTHULHU = document.getElementById('second-monster');
+const IOGSOTHOTH = document.getElementById('third-monster');
+const SHUBNIGGURATH = document.getElementById('fourth-monster');
 const AZATHOTH_INDEX = 0;
-// 1
-// 2
-// 3
-let monsterIndex = AZATHOTH_INDEX;
-// function getMonsterIndex, on card click we making index. And making monsterIndex = mosterIndex
+const CTHULHU_INDEX = 1;
+const IOGSOTHOTH_INDEX = 2;
+const SHUBNIGGURATH_INDEX = 3;
+let monsterIndex;
+let finalCards;
 
-// добавляем цифры в счетчике
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const four = document.getElementById("4");
-const five = document.getElementById("5");
-const six = document.getElementById("6");
-const seven = document.getElementById("7");
-const eight = document.getElementById("8");
-const nine = document.getElementById("9");
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
 
-setValues(ancientsData, monsterIndex);
-
-const cardsQuantity = getCardsQuantity(ancientsData, monsterIndex);
-
-const allCards = getAllCards(greenCards, blueCards, brownCards, DIFFICULTY_LEVEL_NORMAL, cardsQuantity);
-
-const finalCards = getFinalCards(allCards, ancientsData, monsterIndex);
-
-function getCardsQuantity(ancientsData, ancientIndex) {
-  const cardsQuantity = [0, 0, 0];
-
-  const firstStageValues = Object.values(ancientsData[ancientIndex].firstStage);
-  const secondStageValues = Object.values(ancientsData[ancientIndex].secondStage);
-  const thirdStageValues = Object.values(ancientsData[ancientIndex].thirdStage);
-
-  return cardsQuantity.map((value, index) => value + firstStageValues[index] + secondStageValues[index] + thirdStageValues[index]);
+// Добавление функционала на клики
+function getCardsByMonster(ancientsData, monsterIndex, difficulty, greenCards, blueCards, brownCards) {
+    const cardsQuantity = getCardsQuantity(ancientsData, monsterIndex);
+    const allCards = getAllCards(greenCards, blueCards, brownCards, difficulty, cardsQuantity);
+    finalCards = getFinalCards(allCards, ancientsData, monsterIndex);
+    setValues(ancientsData, monsterIndex);
+    document.querySelector('.stages_and_cards_container').style.display = 'flex';
 }
 
-function getAllCards(greenCards, blueCards, brownCards, difficulty, cardsQuantity) {
-  return {
-    greenCards: getCardsSetByColorAndDifficulty(greenCards, difficulty, cardsQuantity[0]),
-    blueCards: getCardsSetByColorAndDifficulty(blueCards, difficulty, cardsQuantity[1]),
-    brownCards: getCardsSetByColorAndDifficulty(brownCards, difficulty, cardsQuantity[2])
-  };
+AZATHOTH.onclick = (event) => {
+    event.target.classList.add('active');
+    getCardsByMonster(ancientsData, AZATHOTH_INDEX, DIFFICULTY_LEVEL_NORMAL, greenCards, blueCards, brownCards);
 };
+CTHULHU.onclick = () =>
+    getCardsByMonster(ancientsData, CTHULHU_INDEX, DIFFICULTY_LEVEL_NORMAL, greenCards, blueCards, brownCards);
+IOGSOTHOTH.onclick = () =>
+    getCardsByMonster(ancientsData, IOGSOTHOTH_INDEX, DIFFICULTY_LEVEL_NORMAL, greenCards, blueCards, brownCards);
+SHUBNIGGURATH.onclick = () =>
+    getCardsByMonster(ancientsData, SHUBNIGGURATH_INDEX, DIFFICULTY_LEVEL_NORMAL, greenCards, blueCards, brownCards);
 
-// функция, которая принимает массив по цвету, фильтрует его по сложности, и выводит перемешанный массив нужного количества
-function getCardsSetByColorAndDifficulty(color, difficulty, quantity) {
-  return shuffle(color.filter(card => card.difficulty === difficulty)).slice(0, quantity);
-};
+// document.querySelector('.normal').onclick = function(e) {
+//   document.querySelector('.mix').style.display = 'flex';
+// };
 
-// распределение по стейджам
-function getFinalCards(cards, ancientsData, ancientIndex) {
-  const firstStageCards = getCardsByStage(cards, ancientsData[ancientIndex].firstStage);
-  const secondStageCards = getCardsByStage(cards, ancientsData[ancientIndex].secondStage);
-  const thirdStageCards = getCardsByStage(cards, ancientsData[ancientIndex].thirdStage);
+// document.querySelector('.mix').onclick = function(e) {
+//   setValues(ancientsData, monsterIndex);
+//   document.querySelector('.stages_and_cards_container').style.display = 'flex';
+// };
 
-  return thirdStageCards.concat(secondStageCards, firstStageCards);
-};
-
-function getCardsByStage(cards, stage) {
-  return shuffle(cards.greenCards.splice(0, stage.greenCards).concat(cards.blueCards.splice(0, stage.blueCards), cards.brownCards.splice(0, stage.brownCards)));
-};
+// document.querySelector('.card_back_side').onclick = function(e) {
+//   document.querySelector('.card_opened_now').style.display = 'block';
+// };
 
 // устанавливаем значения в стейдж
-
 function setValues(ancientsData, ancientIndex) {
-  one.innerHTML = ancientsData[ancientIndex].firstStage.greenCards;
-  two.innerHTML = ancientsData[ancientIndex].firstStage.blueCards;
-  three.innerHTML = ancientsData[ancientIndex].firstStage.brownCards;
-  four.innerHTML = ancientsData[ancientIndex].secondStage.greenCards;
-  five.innerHTML = ancientsData[ancientIndex].secondStage.blueCards;
-  six.innerHTML = ancientsData[ancientIndex].secondStage.brownCards;
-  seven.innerHTML = ancientsData[ancientIndex].thirdStage.greenCards;
-  eight.innerHTML = ancientsData[ancientIndex].thirdStage.blueCards;
-  nine.innerHTML = ancientsData[ancientIndex].thirdStage.brownCards;
-};
+    one.innerHTML = ancientsData[ancientIndex].firstStage.greenCards;
+    two.innerHTML = ancientsData[ancientIndex].firstStage.blueCards;
+    three.innerHTML = ancientsData[ancientIndex].firstStage.brownCards;
+    four.innerHTML = ancientsData[ancientIndex].secondStage.greenCards;
+    five.innerHTML = ancientsData[ancientIndex].secondStage.blueCards;
+    six.innerHTML = ancientsData[ancientIndex].secondStage.brownCards;
+    seven.innerHTML = ancientsData[ancientIndex].thirdStage.greenCards;
+    eight.innerHTML = ancientsData[ancientIndex].thirdStage.blueCards;
+    nine.innerHTML = ancientsData[ancientIndex].thirdStage.brownCards;
+}
 
 // достаем карты по очереди по одной, убираем из колоды, отнимаем число
-
 function showCard() {
-  const frontImage = document.querySelector('.card_opened_now');
-  const backImage = document.querySelector('.card_back_side');
+    const frontImage = document.querySelector('.card_opened_now');
+    const backImage = document.querySelector('.card_back_side');
 
-  if (finalCards.length) {
-    const element = finalCards.pop();
-    console.log(element);
+    document.querySelector('.card_opened_now').style.display = 'block';
 
-    frontImage.src = element.cardFace;
+    if (finalCards.length) {
+        const element = finalCards.pop();
+        console.log(element);
 
-    if (element.id.includes('green')) {
-      delValues(one, four, seven);
-    } else if (element.id.includes('blue')) {
-      delValues(two, five, eight);
-    } else if (element.id.includes('brown')) {
-      delValues(three, six, nine);
+        frontImage.src = element.cardFace;
+
+        if (element.id.includes('green')) {
+            delValues(one, four, seven);
+        } else if (element.id.includes('blue')) {
+            delValues(two, five, eight);
+        } else if (element.id.includes('brown')) {
+            delValues(three, six, nine);
+        }
+    } else {
+        backImage.style.display = 'none';
+        console.log('here are no more cards left');
     }
+}
 
-  } else {
-    backImage.style.display = 'none';
-    console.log('here are no more cards left')
-  }
-};
-
-const cardBackSide = document.querySelector(".card_back_side");
-cardBackSide.addEventListener("click", showCard);
+const cardBackSide = document.querySelector('.card_back_side');
+cardBackSide.addEventListener('click', showCard);
 
 // удаляем цифры в счетчике
 function delValues(green, blue, brown) {
-  if (+green.innerHTML) {
-    green.innerHTML -= 1;
-  } else if (+blue.innerHTML) {
-    blue.innerHTML -= 1;
-  } else {
-    brown.innerHTML -= 1;
-  }
-};
+    if (+green.innerHTML) {
+        green.innerHTML -= 1;
+    } else if (+blue.innerHTML) {
+        blue.innerHTML -= 1;
+    } else {
+        brown.innerHTML -= 1;
+    }
+}
